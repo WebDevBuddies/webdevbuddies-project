ef60c86c81ef3f5365cda435a5b414377c5034df
Mike Vendetti <m.vendetti@gmail.com>
2018-09-10 12:18:44 -0700

    Laravel and vue are ready to go

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+const app = new Vue({
+    el: '#wdb-app'
+});
---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <link href="/css/app.css" rel="stylesheet">
+        <title>Web Dev Buddies</title>
+    </head>
+
+    <body>
+        <div id="wdb-app">
+            <example-component></example-component>
+        </div>
+    </body>
+    <script src="/js/app.js"></script>
+</html>
