0ed28bf763ec73624875829acedde2c041a9236a
Mike Vendetti <m.vendetti@gmail.com>
2018-09-13 14:31:27 -0700

    User table additions

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('username')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('title');
+            $table->string('skill_level');
             $table->rememberToken();
             $table->timestamps();
         });
